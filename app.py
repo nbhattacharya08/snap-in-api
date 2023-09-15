a4e7af4000174f8513c25b42bfbbd5484b5b0684
nbhattacharya08 <nilanjanbhattacharya22@gmail.com>
2023-09-15 22:49:48 +0530

    meow

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from twilio.twiml.voice_response import VoiceResponse
 
 app = Flask(__name__)
 
+@app.route("/", methods=['GET', 'POST'])
+def meow():
+    return "meow"
 
 @app.route("/answer", methods=['GET', 'POST'])
 def answer_call():
@@ -15,17 +18,21 @@ def answer_call():
     resp.say("Please record your message", voice='Polly.Amy')
 
 
-    resp.record(action="https://cf79-202-142-106-83.ngrok-free.app/handleRecord",timeout=10, transcribe=True , transcribeCallback="https://cf79-202-142-106-83.ngrok-free.app/handleText" , channels=2)
+    resp.record(action=" https://5c1b-202-142-106-83.ngrok-free.app/handleRecord",timeout=10, transcribe=True , transcribeCallback="https://cf79-202-142-106-83.ngrok-free.app/handleText" , channels=2)
     print(resp)
     return str(resp)
 
 @app.route("/handleRecord", methods=['GET', 'POST'])
 def handleRecord():
-    print(request)
-    response=requests.get(request.body['RecordingUrl']+".mp3" , auth={"AC2deea27febf4d49d44979e23c46aad2c","AC2deea27febf4d49d44979e23c46aad2c"})
+    print(request.values)
+    url=dict(request.values)
+    url=url['RecordingUrl']+ ".mp3"
+    print(url)
+    response=requests.get(url , auth=("AC2deea27febf4d49d44979e23c46aad2c","fe472d53fb4ae0b7b0d7b29612a986dc"))
+    print(response)
     if response.status_code == 200:
         # Extract the filename from the URL
-        filename = file_url.split("/")[-1]
+        filename = url.split("/")[-1]
 
         # Specify the local path where you want to save the downloaded file
         local_path = f'./{filename}'  # You can customize this path
