0711e4a37b2d4d27a33c0f006063f7831d3b3c10
nbhattacharya08 <nilanjanbhattacharya22@gmail.com>
2023-09-15 21:58:28 +0530

    lmao

--- app.py
+++ app.py
@@ -1,17 +1,43 @@
-# Download the helper library from https://www.twilio.com/docs/python/install
-import os
-from twilio.rest import Client
+from flask import Flask , request
+import requests
+from twilio.twiml.voice_response import VoiceResponse
 
-# Set environment variables for your credentials
-# Read more at http://twil.io/secure
+app = Flask(__name__)
 
-account_sid = "AC2deea27febf4d49d44979e23c46aad2c"
-auth_token = "fe472d53fb4ae0b7b0d7b29612a986dc"
-client = Client(account_sid, auth_token)
 
-call = client.calls.create(
-  url="http://demo.twilio.com/docs/voice.xml",
-  to="+918981829798",
-  from_="+12564856295"
-)
-print(call.sid)
+@app.route("/answer", methods=['GET', 'POST'])
+def answer_call():
+    """Respond to incoming phone calls with a brief message."""
+    # Start our TwiML response
+    resp = VoiceResponse()
+
+    # Read a message aloud to the caller
+    resp.say("Please record your message", voice='Polly.Amy')
+
+
+    resp.record(action="https://cf79-202-142-106-83.ngrok-free.app/handleRecord",timeout=10, transcribe=True , transcribeCallback="https://cf79-202-142-106-83.ngrok-free.app/handleText" , channels=2)
+    print(resp)
+    return str(resp)
+
+@app.route("/handleRecord", methods=['GET', 'POST'])
+def handleRecord():
+    print(request)
+    response=requests.get(request.body['RecordingUrl']+".mp3" , auth={"AC2deea27febf4d49d44979e23c46aad2c","AC2deea27febf4d49d44979e23c46aad2c"})
+    if response.status_code == 200:
+        # Extract the filename from the URL
+        filename = file_url.split("/")[-1]
+
+        # Specify the local path where you want to save the downloaded file
+        local_path = f'./{filename}'  # You can customize this path
+
+        # Write the content of the response (file) to the local file
+        with open(local_path, 'wb') as file:
+            file.write(response.content)
+
+        print(f"File '{filename}' downloaded successfully to '{local_path}'")
+
+    else:
+        print(f"Failed to download file. Status code: {response.status_code}")
+    return "ok"
+if __name__ == "__main__":
+    app.run(debug=True)
