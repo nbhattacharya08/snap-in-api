9f71cb2a62b9458e2bc311d543ca552d538c4b32
techquila42 <55506290+techquila42@users.noreply.github.com>
2023-09-19 01:55:08 +0530

    added endpoints

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask , request
+from flask import Flask , request, jsonify
 import requests
 from twilio.twiml.voice_response import VoiceResponse
+from functions import findIssueMatch
 import os
-from dotenv import load_dotenv
-import openai
+
 load_dotenv()
 
 app = Flask(__name__)
@@ -20,9 +20,15 @@ def answer_call():
 
     # Read a message aloud to the caller
     resp.say("Please record your message", voice='Polly.Amy')
+    dial = Dial(
+    record='record-from-answer-dual',
+    recording_status_callback='https://5c1b-202-142-106-83.ngrok-free.app/handleRecord',
+    timeout=30,
+    channels=2
+    )
+    dial.number('+918336953155')
+    resp.append(dial)
 
-
-    resp.record(action=" https://5c1b-202-142-106-83.ngrok-free.app/handleRecord",timeout=10, transcribe=True , transcribeCallback="https://cf79-202-142-106-83.ngrok-free.app/handleText" , channels=2)
     print(resp)
     return str(resp)
 
@@ -50,14 +56,19 @@ def handleRecord():
 
     else:
         print(f"Failed to download file. Status code: {response.status_code}")
-
+        
     print(os.getenv("OPENAI_API_KEY"))
     openai.api_key = os.getenv("OPENAI_API_KEY")
-
     audio_file= open(f'./{filename}', "rb")
     transcript = openai.Audio.transcribe("whisper-1", audio_file)
     print(transcript)
 
     return transcript["text"]
+
+
+@app.route("/group-issue",methods=["POST"])
+def handleGroupIssue():
+    return jsonify(findIssueMatch(request))
+    
 if __name__ == "__main__":
     app.run(debug=True)
