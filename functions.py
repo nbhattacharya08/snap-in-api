39d52559e84355d5ac79678f64dd7ba994729358
techquila42 <55506290+techquila42@users.noreply.github.com>
2023-09-21 23:50:00 +0530

    openai

--- functions.py
+++ functions.py
@@ -44,7 +44,7 @@ def generateIssue(summary):
   )
   reply=response['choices'][0]['message']['content']
   reply=reply.split('&')
-  return {"issues":reply[0].split('$'), "text":ast.literal_eval(reply[1])}
+  return {"issues":reply[0].split('$'), "text":ast.literal_eval(reply[1].strip())}
 
 
 def findIssueMatch(request):
@@ -109,7 +109,43 @@ def matchesIssues(ticketIssue,issues):
   presence_penalty=0
   )
   reply=response['choices'][0]['message']['content']
-  return ast.literal_eval(reply)
+  return ast.literal_eval(reply.strip())
+
+def generateTicket(text):
+  response = openai.ChatCompletion.create(
+    model="gpt-4",
+    messages=[
+      {
+        "role": "system",
+        "content": "You are given a transcript of a call between a customer care executive and a customer. Infer the customer's dialogues and provide a summary of the call. Then, provide a short title for the summary. The output must be formatted in the following way\n\"Summary $ Title\""
+      },
+      {
+        "role": "user",
+        "content": "Hello. Hello, I am calling about an order on a food delivery app. Yeah, could you tell me what problem you are facing? I can't contact my delivery partner. Alright, just give me a second, I will look into it. Yeah, hello sir, your delivery partner has been assigned and they will contact you shortly. Are you facing any other problem? There were a lot of spelling errors in your website. Alright, fine, I will have my team look at it. Okay. Thank you."
+      },
+      {
+        "role": "assistant",
+        "content": "The customer contacts the customer care executive to report two issues. The first issue is that they are unable to contact their assigned delivery partner. The executive assures the customer that the delivery partner will be in touch soon. The second issue is about spelling errors on the food delivery app's website. The executive acknowledges the problem and assures the customer that their team will address it. The call concludes with the customer expressing their gratitude. $  Food delivery issue and spelling errors."
+      },
+      {
+        "role": "user",
+        "content": "Hello. Hi, I am calling about an issue on a music listening app. Could you tell me what the problem is? I cant view my playlists in the app. We are working on that bug currently, thank you for your patience. Some songs are also not available on the app. That is due to some policy changes, Sir. Okay, Thank you. Bye."
+      },
+      {
+        "role": "assistant",
+        "content": "The customer contacts the customer care executive to report two issues with a music listening app. The first issue is that they are unable to view their playlists in the app. The executive informs the customer that there is a bug currently being worked on and appreciates the customer's patience. The second issue is that some songs are not available on the app, to which the executive explains that it is due to policy changes. The call concludes with the customer expressing gratitude and ending the call. $ Playlist visibility issue and unavailable songs."
+      },
+      {
+        "role": "user",
+        "content": text
+      }
+    ],
+    temperature=1,
+    max_tokens=256,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+  )
 
 
 
